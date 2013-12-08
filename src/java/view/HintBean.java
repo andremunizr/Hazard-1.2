ac716c0894e0958332e9afc54030026188955a3c
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2013-12-08 11:42:39 -0200

    Hints

--- src/java/view/HintBean.java
+++ src/java/view/HintBean.java
@@ -3,6 +3,7 @@ package view;
 import controller.MainController;
 import java.net.UnknownHostException;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -18,6 +19,7 @@ import model.Hint;
 public class HintBean {
 
     private Hint hint;
+    private List<Hint> hints;
     
     @EJB
     private MainController controller;
@@ -25,8 +27,13 @@ public class HintBean {
     @Inject
     private LoggBean logBean;
     
-    public HintBean() {
+    public HintBean() throws UnknownHostException {
         hint = new Hint();
+    }
+    
+    @PostConstruct
+    public void initializer() throws UnknownHostException {
+        hints = findAll();
     }
 
     public Hint getHint() {
@@ -36,9 +43,21 @@ public class HintBean {
     public void setHint(Hint hint) {
         this.hint = hint;
     }
+
+    public List<Hint> getHints() {
+        return hints;
+    }
+
+    public void setHints(List<Hint> hints) {
+        this.hints = hints;
+    }
     
     public List<Hint> findAll() throws UnknownHostException {
         return ( List<Hint> ) ( List<?> ) controller.getDocuments( Hint.class );
+    }
+    
+    public Hint findOne( String id ) throws UnknownHostException {
+        return ( Hint ) controller.findOne( Hint.class, id );
     }
     
     public void save() throws UnknownHostException {
